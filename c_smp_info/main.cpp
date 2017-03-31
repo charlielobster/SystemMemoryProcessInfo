a45212e39823ec2df0dbef65e7312caa5563d5c2
charlielobster <erik.brobyn@icloud.com>
2017-03-30 22:56:35 -0700

    clr wrapper

--- c_smp_info/main.cpp
+++ c_smp_info/main.cpp
@@ -18,6 +18,87 @@ static DWORD processIds[1024];
 static DWORD processCount;
 
 /*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// wrap_native_class_for_mgd_consumption.cpp
+// compile with: /clr /LD
+#include <windows.h>
+#include <vcclr.h>
+#using <System.dll>
+
+using namespace System;
+
+class UnmanagedClass {
+public:
+LPCWSTR GetPropertyA() { return 0; }
+void MethodB( LPCWSTR ) {}
+};
+
+public ref class ManagedClass {
+public:
+// Allocate the native object on the C++ Heap via a constructor
+ManagedClass() : m_Impl( new UnmanagedClass ) {}
+
+// Deallocate the native object on a destructor
+~ManagedClass() {
+delete m_Impl;
+}
+
+protected:
+// Deallocate the native object on the finalizer just in case no destructor is called
+!ManagedClass() {
+delete m_Impl;
+}
+
+public:
+property String ^  get_PropertyA {
+String ^ get() {
+return gcnew String( m_Impl->GetPropertyA());
+}
+}
+
+void MethodB( String ^ theString ) {
+pin_ptr<const WCHAR> str = PtrToStringChars(theString);
+m_Impl->MethodB(str);
+}
+
+private:
+UnmanagedClass * m_Impl;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // pdh stuff
 static PDH_HQUERY cpuQuery;
 static PDH_HCOUNTER cpuTotal;
