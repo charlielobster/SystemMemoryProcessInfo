efa4cecc19d4a3a95576b02fea0c63aaa6ce6301
charlielobster <erik.brobyn@icloud.com>
2017-03-30 23:17:41 -0700

    clr project

---
+++ clr_smp_info/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// clr_smp_info.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
---
+++ clr_smp_info/clr_smp_info.h
@@ -0,0 +1,13 @@
+// clr_smp_info.h
+
+#pragma once
+
+using namespace System;
+
+namespace clr_smp_info {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
